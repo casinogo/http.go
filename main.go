57815935ae5e92f68956fba5d0d3dc363034d092
casinogo <casino.go@icloud.com>
2025-07-10 10:36:54 -0400

    Init update

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	listenr, err := net.Listen("tcp", ":42069")
+	if err != nil {
+		panic(err)
+	}
+	defer listenr.Close()
+
+	conn, err := listenr.Accept()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connection established from", conn.RemoteAddr())
+
+	ch := getLinesChannel(conn)
+
+	for line := range ch {
+		fmt.Println(line)
+	}
+
+	fmt.Println("Connection closed from", conn.RemoteAddr())
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	go func() {
+		defer close(ch)
+		defer f.Close()
+
+		var curLine string
+		buf := make([]byte, 8)
+		for {
+			n, err := f.Read(buf)
+			if err != nil {
+				if err.Error() == "EOF" {
+					break
+				}
+				panic(err)
+			}
+			if n == 0 {
+				break
+			}
+			line := string(buf[0:n])
+			curLine += line
+
+			result := strings.Split(curLine, "\n")
+
+			if len(result) > 1 {
+				for i := 0; i < len(result)-1; i++ {
+					ch <- result[i]
+				}
+				curLine = result[len(result)-1]
+			}
+		}
+		if len(curLine) > 0 {
+			ch <- curLine
+		}
+	}()
+	return ch
+}
